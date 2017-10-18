31c9538a47c62c8926b68bb8dcd97ecfb1c9ae15
Jérôme <jerome.bsr@gmail.com>
2017-10-19 00:59:26 +0200

    Affichage du tableau de jeu [OK]

--- View/Form.php
+++ View/Form.php
@@ -28,19 +28,18 @@ function viewGameBoard()
 {
     $tab  = "<table border='1'>";
     $tab .= "<tr>";
-    $tab .= "<td>a</td>";
-    $tab .= "<td>b</td>";
-    $tab .= "<td>c</td>";
+    $tab .= "<td><a href='index.php?case=a1' title='Choisir la case A1'>A1</a></td>";
+    $tab .= "<td><a href='index.php?case=b1' title='Choisir la case B1'>B1</a></td>";
+    $tab .= "<td><a href='index.php?case=c1' title='Choisir la case C1'>C1</a></td>";
     $tab .= "</tr>";
     $tab .= "<tr>";
-    $tab .= "<td>a</td>";
-    $tab .= "<td>b</td>";
-    $tab .= "<td>c</td>";
-    $tab .= "</tr>";
+    $tab .= "<td><a href='index.php?case=a2' title='Choisir la case A2'>A2</a></td>";
+    $tab .= "<td><a href='index.php?case=b2' title='Choisir la case B2'>B2</a></td>";
+    $tab .= "<td><a href='index.php?case=c2' title='Choisir la case C2'>C2</a></td>";
     $tab .= "<tr>";
-    $tab .= "<td>a</td>";
-    $tab .= "<td>b</td>";
-    $tab .= "<td>c</td>";
+    $tab .= "<td><a href='index.php?case=a3' title='Choisir la case A3'>A3</a></td>";
+    $tab .= "<td><a href='index.php?case=b3' title='Choisir la case B3'>B3</a></td>";
+    $tab .= "<td><a href='index.php?case=c3' title='Choisir la case C3'>C3</a></td>";
     $tab .= "</tr>";
     $tab .= "</table>";
 
