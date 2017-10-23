abf59c76d96dae61c7d0752e7b468ff43c9c691b
Jérôme <jerome.bsr@gmail.com>
2017-10-23 02:14:23 +0200

    Màj CSS

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
 <head>
     <meta charset="utf-8">
     <title>Jeu du Morpion</title>
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
-    <link rel="stylesheet" href="View/css/style.css" />
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous" />
+    <link rel="stylesheet" href="View/css/custom.css" />
     <script type="text/javascript" src="jquery-3.2.1.js"> </script>
     <script type="text/javascript" src="message.js"> </script>
 </head>
